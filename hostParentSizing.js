42bf839bfca7b0628b2bb2574d72da43ddde263f
Aspa7beginner <65359456+Aspa7beginner@users.noreply.github.com>
2023-01-13 15:42:46 +0200

    Update hostParentSizing.js

--- hostParentSizing.js
+++ hostParentSizing.js
@@ -1,4 +1,8 @@
-  let height;
+/*
+used in README.md file for getting its height size and sending it to the hosting size (my main webpage),
+in order to fix the size of the iframe on the parent hosting site the contains it.
+*/  
+let height;
 
   const sendPostMessage = () => {
     if (height !== document.getElementByClassName("markdown-body")[0].offsetHeight) {
@@ -12,3 +16,7 @@
 
   window.onload = () => sendPostMessage();
   window.onresize = () => sendPostMessage();
+/*
+remember to visit (https://github.com/ryanflorence/render-markdown-javascript/blob/master/README.md)
+in order to find out how the embed js code into README.md files and look for a safer option
+*/
