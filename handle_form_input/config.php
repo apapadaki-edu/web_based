a2bee73a13429a05a4a881139fc8bd0467078881
apapadaki-edu <65359456+apapadaki-edu@users.noreply.github.com>
2023-03-14 09:35:43 +0200

    Update config.php

--- handle_form_input/config.php
+++ handle_form_input/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DBHOST', 'localhost');
 define('DBNAME', 'my_art');
-define('DBUSER', 'webuser@localhost');
-define('DBPASS', '12345678');
+define('DBUSER', 'admin@localhost');
+define('DBPASS', 'temppass');
 define('DBCONNSTRING','pgsql:host=localhost;port=5432;dbname=my_art');
 ?>
